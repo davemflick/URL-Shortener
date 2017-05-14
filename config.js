4192efe0ff5a729e412e879f56c0fffa8612892d
davemflick <davemflick@yahoo.com>
2017-05-14 09:42:51 -0400

    readded config file and changed listen

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.db = {};
+
+config.db.host = 'mongodb://davemflick:finland@ds139761.mlab.com:39761';
+config.db.name = 'url-shortener-microservice';
+
+module.exports = config;
