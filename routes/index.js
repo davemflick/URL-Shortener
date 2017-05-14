2473b9ba9d7bb399bde621e0ae6c0c8f9e26e75a
davemflick <davemflick@yahoo.com>
2017-05-14 08:37:55 -0400

    connected and working with mongo and mLab. No style

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 var mongodb = require('mongodb');
 var shortid = require('shortid');
 var validUrl = require('valid-url');
+var config = require('../config');
 
 //Set up URL that points to database
-var url = 'mongodb://localhost:27017/url-shortener-microservice'; //Add /databaseName when created
+var url = 'mongodb://' + config.db.host + '/' + config.db.name; //Add /databaseName when created
 var MongoClient = mongodb.MongoClient;
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,24 +15,72 @@ router.get('/', function(req, res, next) {
 
 //GET url
 router.get('/new/:url(*)', function (req, res, next) {
-	MongoClient.connect(url, function (err, db) {
-	  if (err) {
-	    console.log("Unable to connect to server", err);
-	  } else {
-	    console.log("Connected to server");
-	    var collection = db.collection('links');
-		var params = req.params.url;
-		var newLink = function (db, callback) {
-			var insertLink = { url: params, short: "test" };
-			collection.insert([insertLink]);
-			res.send(params);
-		};
+	  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log("Unable to connect to server", err);
+    } else {
+      console.log("Connected to server")
+
+      var collection = db.collection('links');
+      var params = req.params.url;
+
+      //sets current hostname to var local
+      var local = req.get('host') + "/";
+
+      var newLink = function (db, callback) {
+        collection.findOne({ "url": params }, { short: 1, _id: 0 }, function (err, doc) {
+          if (doc != null) {
+            res.json({ original: params, short: local + doc.short });
+          } else {
+            if (validUrl.isUri(params)) {
+              // if URL is valid, do this
+              var shortCode = shortid.generate();
+              var newUrl = { url: params, short: shortCode };
+              collection.insert([newUrl]);
+              res.json({ original: params, short: local + shortCode });
+            } else {
+            // if URL is invalid, do this
+              res.json({ error: "Incorrect url format." });
+            };
+          };
+        });
+      };
 
 		newLink(db, function () {
 		  db.close();
 		});
 	  };
 	});
+});
+
+
+router.get('/:short', function (req, res, next) {
+
+    MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log("Unable to connect to server", err);
+    } else {
+      console.log("Connected to server")
+
+      var collection = db.collection('links');
+      var params = req.params.short;
+
+      var findLink = function (db, callback) {
+        collection.findOne({ "short": params }, { url: 1, _id: 0 }, function (err, doc) {
+          if (doc != null) {
+            res.redirect(doc.url);
+          } else {
+            res.json({ error: "No shortlink database." });
+          };
+        });
+      };
+
+      findLink(db, function () {
+        db.close();
+      });
+
+    };
+  });
 });
 
 
